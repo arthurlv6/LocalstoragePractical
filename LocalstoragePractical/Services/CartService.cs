13cfa97018c6c18261571970ebae21482f8fbd02
Arthur Lu <arthur.lv6@gmail.com>
2020-10-13 16:16:26 +1300

    Optimized UI.

--- LocalstoragePractical/Services/CartService.cs
+++ LocalstoragePractical/Services/CartService.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 
 namespace LocalstoragePractical.Services
 {
-  public class CartService : ICartService
-  {
-    public event EventHandler<CartChangedEventArgs> CartChanged;
-
-    public Cart Cart { get; set; } = new Cart
+    public class CartService : ICartService
     {
-     Items=new List<Item>() 
-     { 
-         new Item { Name="123",Price=12.2,Quantity=1 },
-         new Item { Name="34",Price=12.2,Quantity=1 },
-         new Item { Name="255",Price=12.2,Quantity=1 }
-     }
-    };
+        public event EventHandler<CartChangedEventArgs> CartChanged;
 
-    public void RaiseCartChanged()
-    {
+        public Cart Cart { get; set; } = new Cart
+        {
+            Items = new List<Item>()
+             {
+                 new Item { Name="123",Price=12.2,Quantity=1 },
+                 new Item { Name="34",Price=12.2,Quantity=1 },
+                 new Item { Name="255",Price=12.2,Quantity=1 }
+             }
+        };
+
+        public void RaiseCartChanged()
+        {
             CartChanged?.Invoke(this, new CartChangedEventArgs(Cart));
+        }
+        
     }
-  }
 }
